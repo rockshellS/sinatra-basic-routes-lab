454c9733d92408c89fb87cd0f38c897d8aac49ea
rockshellS <spicer.rachelle@gmail.com>
2021-01-11 16:09:32 -0600

    final changes

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Rachelle Spicer"
+    end
+    
+    get '/hometown' do
+        "My hometown is Brighton"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Happy Idiot"
+    end
+
 end
